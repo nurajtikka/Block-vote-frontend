a89290063506f00ad06fb607b90be5a88afa4111
Minhaz Irphan Mohamed <MinhazI@users.noreply.github.com>
2023-07-12 21:20:29 +0530

    Updated the UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,90 +1,175 @@
-'use client';
+// 'use client';
 
-import React from 'react';
+// import React from 'react';
 
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { useRouter } from 'next/navigation';
-import { Layout, theme } from 'antd';
+// import { QueryClient, QueryClientProvider } from 'react-query';
+// import { useRouter } from 'next/navigation';
+// import { Layout, theme } from 'antd';
 
-import styles from './layout.module.scss';
-import '../styles/theme.scss';
-import { ProvideAppContext } from './contexts/AppContext';
+// import styles from './layout.module.scss';
+// import '../styles/theme.scss';
+// import { ProvideAppContext } from './contexts/AppContext';
 
-const { Content } = Layout;
+// const { Content } = Layout;
 
-const RootLayout = ({ children }: { children: React.ReactNode }): React.ReactNode => {
-    const {
-        token: { colorBgContainer },
-    } = theme.useToken();
+// const RootLayout = ({ children }: { children: React.ReactNode }): React.ReactNode => {
+//     const {
+//         token: { colorBgContainer },
+//     } = theme.useToken();
 
-    // Example of using react-query as a data provider
-    const [queryClient] = React.useState(
-        () =>
-            new QueryClient({
-                defaultOptions: {
-                    queries: {
-                        refetchOnWindowFocus: false,
-                        retry: false,
-                    },
-                },
-            }),
-    );
+//     // Example of using react-query as a data provider
+//     const [queryClient] = React.useState(
+//         () =>
+//             new QueryClient({
+//                 defaultOptions: {
+//                     queries: {
+//                         refetchOnWindowFocus: false,
+//                         retry: false,
+//                     },
+//                 },
+//             }),
+//     );
 
-    const router = useRouter();
+//     const router = useRouter();
 
-    return (
-        <html lang="en" dir="ltr">
-            <head>
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-                {/* eslint-disable-next-line @next/next/no-page-custom-font */}
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Noto+Sans+Arabic:wght@400;600;700&family=Noto+Sans:wght@400;600;700&display=swap"
-                    rel="stylesheet"
-                />
-                <title>Block-Vote</title>
-            </head>
-            <body>
-                {/* Added app context provider here */}
-                <ProvideAppContext>
-                    <Layout className="layout">
-                        {/* <Header>
-                        <div className="logo" />
-                        <Menu
-                            theme="dark"
-                            mode="horizontal"
-                            defaultSelectedKeys={['2']}
-                            items={new Array(3).fill(null).map((_, index) => {
-                                const key = index + 1;
-                                return {
-                                    key,
-                                    label: `nav ${key}`,
-                                };
-                            })}
-                        />
-                    </Header> */}
-                        <Content style={{ background: 'white' }}>
-                            {/* <Breadcrumb style={{ 'margin: '16px 0' }}>
-                            <Breadcrumb.Item>Home</Breadcrumb.Item>
-                            <Breadcrumb.Item>List</Breadcrumb.Item>
-                            <Breadcrumb.Item>App</Breadcrumb.Item>
-                        </Breadcrumb> */}
-                            <div>
-                                <QueryClientProvider client={queryClient}>
-                                    <main className={styles.site}>
-                                        <div className={styles.siteContent}>{children}</div>
-                                    </main>
-                                </QueryClientProvider>{' '}
-                            </div>
-                            {/* <Button>
-                            <Link href="/orders">English</Link>
-                        </Button> */}
-                        </Content>
-                    </Layout>
-                </ProvideAppContext>
-            </body>
-        </html>
-    );
-};
+//     return (
+//         <html lang="en" dir="ltr">
+//             <head>
+//                 <link rel="preconnect" href="https://fonts.googleapis.com" />
+//                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+//                 {/* eslint-disable-next-line @next/next/no-page-custom-font */}
+//                 <link
+//                     href="https://fonts.googleapis.com/css2?family=Noto+Sans+Arabic:wght@400;600;700&family=Noto+Sans:wght@400;600;700&display=swap"
+//                     rel="stylesheet"
+//                 />
+//                 <title>Block-Vote</title>
+//             </head>
+//             <body>
+//                 {/* Added app context provider here */}
+//                 <ProvideAppContext>
+//                     <Layout className="layout">
+//                         {/* <Header>
+//                         <div className="logo" />
+//                         <Menu
+//                             theme="dark"
+//                             mode="horizontal"
+//                             defaultSelectedKeys={['2']}
+//                             items={new Array(3).fill(null).map((_, index) => {
+//                                 const key = index + 1;
+//                                 return {
+//                                     key,
+//                                     label: `nav ${key}`,
+//                                 };
+//                             })}
+//                         />
+//                     </Header> */}
+//                         <Content style={{ background: 'white' }}>
+//                             {/* <Breadcrumb style={{ 'margin: '16px 0' }}>
+//                             <Breadcrumb.Item>Home</Breadcrumb.Item>
+//                             <Breadcrumb.Item>List</Breadcrumb.Item>
+//                             <Breadcrumb.Item>App</Breadcrumb.Item>
+//                         </Breadcrumb> */}
+//                             <div>
+//                                 <QueryClientProvider client={queryClient}>
+//                                     <main className={styles.site}>
+//                                         <div className={styles.siteContent}>{children}</div>
+//                                     </main>
+//                                 </QueryClientProvider>{' '}
+//                             </div>
+//                             {/* <Button>
+//                             <Link href="/orders">English</Link>
+//                         </Button> */}
+//                         </Content>
+//                     </Layout>
+//                 </ProvideAppContext>
+//             </body>
+//         </html>
+//     );
+// };
 
-export default RootLayout;
+// export default RootLayout;
+
+"use client";
+
+import React, { useState } from "react";
+import "../styles/globals.scss";
+import { useRouter } from "next/navigation";
+
+import { AreaChartOutlined, DatabaseOutlined } from "@ant-design/icons";
+import { Layout, Menu, theme, Typography } from "antd";
+import type { MenuProps } from "antd";
+import Image from "next/image";
+
+const { Header, Sider, Content } = Layout;
+const { Title } = Typography;
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const router = useRouter();
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+  const [current, setCurrent] = useState<string>("dashboard");
+
+  const onClick: MenuProps["onClick"] = (e) => {
+    setCurrent(e.key);
+    router.push(`/pages/${e.key}`);
+  };
+
+  return (
+    <html lang="en">
+      <body>
+        <Layout>
+          <Sider collapsed={false}>
+            <div className="demo-logo-vertical">
+              <Image
+                src="/logo.jpg"
+                alt="Block Vote Logo"
+                width={195}
+                height={95}
+              />
+            </div>
+            <Menu
+              theme="dark"
+              mode="inline"
+              onClick={onClick}
+              defaultSelectedKeys={["dashboard"]}
+              selectedKeys={[current]}
+            //   items={[
+            //     {
+            //       key: "dashboard",
+            //       icon: <AreaChartOutlined />,
+            //       label: "Dashboard",
+            //     },
+            //     {
+            //       key: "campaignData",
+            //       icon: <DatabaseOutlined />,
+            //       label: "Campaign Data",
+            //     },
+            //   ]}
+            />
+          </Sider>
+          <Layout>
+            {/* <Header style={{ padding: "0 15px", background: colorBgContainer }}>
+              <Title level={2} style={{ color: "gray" }}>
+                {current === "dashboard" ? "Dashboard" : "Campaign Data"}
+              </Title>
+            </Header> */}
+            <Content
+              style={{
+                margin: "24px 16px",
+                minHeight: 280,
+                background: "#f7f7fa",
+              }}
+            >
+              {children}
+            </Content>
+          </Layout>
+        </Layout>
+      </body>
+    </html>
+  );
+}
--- src/app/pages/scanBarcode/page.tsx
+++ src/app/pages/scanBarcode/page.tsx
@@ -1,131 +1,157 @@
-'use client';
+"use client";
 
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useState } from "react";
 
-import Image from 'next/image';
-import { Col, Row, Button, Input, Typography } from 'antd';
-import { useRouter } from 'next/navigation';
+import Image from "next/image";
+import { Col, Row, Button, Input, Typography } from "antd";
+import { useRouter } from "next/navigation";
 
-import { ArrowLeftOutlined } from '@ant-design/icons';
+import { ArrowLeftOutlined } from "@ant-design/icons";
 
-import flag from '../../assets/flag.png';
-import flag1 from '../../assets/flag1.png';
-import useAppContext from '../../contexts/AppContext';
+import flag from "../../assets/flag.png";
+import flag1 from "../../assets/flag1.png";
+import useAppContext from "../../contexts/AppContext";
 
 const LanguagePage = () => {
-    const router = useRouter();
-    const language = sessionStorage.getItem('block-vote-language');
-    const [nicNumber, setNicNumber] = useState<string>('');
-    const [allowContinue, setAllowContinue] = useState<boolean>(false);
-    const [allowToContiue, setAllowToContinue] = useState<boolean>(false);
-    const [loading, setLoading] = useState<boolean>(false);
-    const [error, setError] = useState<string>('');
-    const { Text } = Typography;
-    const { eligible, isLoading, setNic, votes } = useAppContext();
+  const router = useRouter();
+  const language = sessionStorage.getItem("block-vote-language");
+  const [nicNumber, setNicNumber] = useState<string>("");
+  const [allowContinue, setAllowContinue] = useState<boolean>(false);
+  const [allowToContiue, setAllowToContinue] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
+  const { Text } = Typography;
+  const { eligible, isLoading, setNic, votes } = useAppContext();
 
-    const changeInput = (nic: ChangeEvent<HTMLInputElement>) => {
-        setNicNumber(nic.target.value);
+  const changeInput = (nic: ChangeEvent<HTMLInputElement>) => {
+    setNicNumber(nic.target.value);
+    setAllowContinue(true);
 
-        if (nic.target.value.length >= 10 && nic.target.value.length <= 13) {
-            setAllowContinue(true);
-        } else {
-            setAllowContinue(false);
-        }
-    };
+    if (nic.target.value.length >= 10 && nic.target.value.length <= 13) {
+      setAllowContinue(true);
+    } else {
+      setAllowContinue(false);
+    }
+  };
 
-    const continueToNext = () => {
-        setLoading(true);
-        setError('');
-        setNic(nicNumber);
-        setLoading(false);
-    };
+  const continueToNext = () => {
+    setLoading(true);
+    setError("");
+    setNic(nicNumber);
+    setLoading(false);
+  };
 
-    return (
-        <main
-            className="flex min-h-screen flex-col items-center justify-between p-24 test"
-            style={{ marginLeft: '70px', marginTop: '30px', marginRight: '70px' }}
+  return (
+    <main
+      className="flex min-h-screen flex-col items-center justify-between p-24 test"
+      style={{
+        marginLeft: "70px",
+        marginTop: "30px",
+        marginRight: "70px",
+        minHeight: "100vh",
+      }}
+    >
+      <Row className="text-center">
+        <Col span={12} style={{ textAlign: "left" }}>
+          {" "}
+          <Image src={flag} alt="flag" className="" width={200} priority />
+        </Col>
+        <Col span={12}>
+          {" "}
+          <Image
+            style={{ float: "right", marginRight: "70px" }}
+            src={flag1}
+            alt="flag"
+            className=""
+            width={100}
+            priority
+          />
+        </Col>
+      </Row>
+
+      <Row
+        className="relative flex place-items-center mt-28"
+        style={{ textAlign: "center", justifyContent: "center" }}
+      >
+        {error.length > 1 && (
+          <Col span={24}>
+            <Typography.Title
+              className="mt-4 heading text-center"
+              level={1}
+              type="danger"
+            >
+              {error}
+            </Typography.Title>
+          </Col>
+        )}
+        <Col span={24}>
+          <Typography.Text
+            className="mt-4 subHeading text-center"
+            style={{ fontSize: "32px", marginTop: "8%" }}
+          >
+            {(language === "en" && "Please enter your NIC number.") ||
+              (language === "ta" && "உங்கள் NIC எண்ணை உள்ளிடவும்.") ||
+              (language === "si" &&
+                "කරුණාකර ඔබගේ ජාතික හැඳුනුම්පත් අංකය ඇතුලත් කරන්න.")}
+          </Typography.Text>
+        </Col>
+        <Col
+          span={12}
+          className="flex place-items-center "
+          style={{
+            textAlign: "center",
+            marginTop: "5%",
+            justifyContent: "center",
+          }}
         >
-            <Row className="text-center">
-                <Col span={12} style={{ textAlign: 'left' }}>
-                    {' '}
-                    <Image src={flag} alt="flag" className="" width={200} priority />
-                </Col>
-                <Col span={12}>
-                    {' '}
-                    <Image
-                        style={{ float: 'right', marginRight: '70px' }}
-                        src={flag1}
-                        alt="flag"
-                        className=""
-                        width={100}
-                        priority
-                    />
-                </Col>
-            </Row>
+          <Input
+            placeholder={
+              (language === "en" && "Please enter your NIC number") ||
+              (language === "ta" && "உங்கள் NIC எண்ணை உள்ளிடவும்") ||
+              (language === "si" &&
+                "කරුණාකර ඔබගේ ජාතික හැඳුනුම්පත් අංකය ඇතුලත් කරන්න") ||
+              ""
+            }
+            value={nicNumber}
+            onChange={(nic) => {
+              changeInput(nic);
+            }}
+            maxLength={13}
+            style={{ textAlign: "center" }}
+          />
+        </Col>
+        <Col span={24} style={{ textAlign: "center", marginTop: "5%" }}>
+          <Button
+            className="ant-btn-outline-white"
+            size="large"
+            onClick={() => continueToNext()}
+            disabled={!allowContinue}
+            loading={loading}
+          >
+            {(language === "en" && "Begin") ||
+              (language === "ta" && "தொடங்கு") ||
+              (language === "si" && "ආරම්භ කරන්න")}
+          </Button>
+        </Col>
+      </Row>
 
-            <Row className="relative flex place-items-center mt-28" style={{ textAlign: 'center' }}>
-                {error.length > 1 && (
-                    <Col span={24}>
-                        <Typography.Title className="mt-4 heading text-center" level={1} type="danger">
-                            {error}
-                        </Typography.Title>
-                    </Col>
-                )}
-                <Col span={24}>
-                    <p className="mt-4 subHeading text-center" style={{ fontSize: '32px', marginTop: '8%' }}>
-                        {(language === 'en' && 'Please enter your NIC number.') ||
-                            (language === 'ta' && 'உங்கள் NIC எண்ணை உள்ளிடவும்.') ||
-                            (language === 'si' && 'කරුණාකර ඔබගේ ජාතික හැඳුනුම්පත් අංකය ඇතුලත් කරන්න.')}
-                    </p>
-                </Col>
-                <Col span={24} style={{ textAlign: 'center', marginTop: '5%' }}>
-                    <Input
-                        placeholder={
-                            (language === 'en' && 'Please enter your NIC number') ||
-                            (language === 'ta' && 'உங்கள் NIC எண்ணை உள்ளிடவும்') ||
-                            (language === 'si' && 'කරුණාකර ඔබගේ ජාතික හැඳුනුම්පත් අංකය ඇතුලත් කරන්න') ||
-                            ''
-                        }
-                        value={nicNumber}
-                        onChange={(nic) => {
-                            changeInput(nic);
-                        }}
-                        maxLength={13}
-                        style={{ textAlign: 'center' }}
-                    />
-                </Col>
-                <Col span={24} style={{ textAlign: 'center', marginTop: '5%' }}>
-                    <Button
-                        className="ant-btn-outline-white"
-                        size="large"
-                        onClick={() => continueToNext()}
-                        disabled={!allowContinue || isLoading}
-                        loading={loading}
-                    >
-                        {(language === 'en' && 'Confirm') ||
-                            (language === 'ta' && 'உறுதிப்படுத்தவும்') ||
-                            (language === 'si' && 'තහවුරු කරන්න')}
-                    </Button>
-                </Col>
-            </Row>
-
-            <Row style={{ marginTop: '10%' }}>
-                <Button
-                    className="backBtn"
-                    size="large"
-                    type="dashed"
-                    icon={<ArrowLeftOutlined rev="" />}
-                    onClick={() => {
-                        router.push('/pages/language');
-                    }}
-                >
-                    {(language === 'en' && 'Back') ||
-                        (language === 'ta' && 'மீண்டும்') ||
-                        (language === 'si' && 'ආපසු')}
-                </Button>
-            </Row>
-        </main>
-    );
+      <Row style={{ marginTop: "10%" }}>
+        <Button
+          className="backBtn"
+          size="large"
+          type="dashed"
+          icon={<ArrowLeftOutlined rev="" />}
+          onClick={() => {
+            router.push("/pages/language");
+          }}
+        >
+          {(language === "en" && "Back") ||
+            (language === "ta" && "மீண்டும்") ||
+            (language === "si" && "ආපසු")}
+        </Button>
+      </Row>
+    </main>
+  );
 };
 
 export default LanguagePage;
